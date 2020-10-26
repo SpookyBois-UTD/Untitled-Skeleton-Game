53793a378f2018d62970e7a71fe7e99ac7f229ad
Gemaris <gxv170430@utdallas.edu>
2020-10-26 02:59:25 -0500

    Added a main game scene and renamed the original test scene

---
+++ MainScene.gd
@@ -0,0 +1,26 @@
+extends Node2D
+"""
+This is where the main game loop will lay and the stuff that the player can see will reside in here
+After scene (class) is created, a node will can be placed in this main scene for the game
+This includes:
+	Level
+	Skeleton boi
+	Enemies
+	Items
+	Etc.
+There is nothing right now, this is just a comment to show what this does since nothing exists in it at the moment
+"""
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
